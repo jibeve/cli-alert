d05f801a0b2e38b6620b7ba4ac87eb6eaaeb0724
jibe <jeanbaptiste.voillemin@gmail.com>
2023-01-17 11:21:56 +0100

    Small changes to test and index

--- index.js
+++ index.js
@@ -2,35 +2,43 @@
  * @author jibeve <https://www.pixelforthemasses.com>
  */
 
-const chalk = require('chalk');
-const sym = require('log-symbols');
-
+const chalk = require('chalk'); //Chalk module
+const sym = require('log-symbols'); //Sym module
+const green = chalk.green;
+const greenI = chalk.green.inverse;
+const red = chalk.red;
+const redI = chalk.red.bold.inverse;
+const orange = chalk.keyword('orange');
+const orangeI = chalk.keyword('orange').inverse;
+const blue = chalk.blue;
+const blueI = chalk.blue.inverse;
 
 module.exports = (options) => {
-    //console.log(`WORKING`);
+    
     const defaultOptions = {
             type: 'error',
-            msg: 'You forgot to define options',
+            msg: 'You forgot to define all options',
             name: ""
     };
     const opts = {...defaultOptions, ...options};
     const {type, msg, name} = opts;
 
+    //If name is define, print the name, else uppercase the type as name
     const printName = name ? name : type.toUpperCase();
 
     if(type === 'success'){
-        console.log( `\n ${sym.success} ${chalk.green.inverse(msg)}\n` )
+        console.log( `\n ${sym.success} ${greenI(`  ${printName} `)} ${green(msg)}\n` )
     }
 
     if(type === 'error'){
-        console.log(`\n ${sym.error} ${chalk.red.bold.inverse(msg)}\n`);
+        console.log(`\n ${sym.error} ${redI(` ${printName} `)} ${red(msg)}\n`);
     }
 
     if(type === 'warning'){
-        console.log(`\n ${sym.warning} ${chalk.yellow.bold.inverse(msg)}\n`);
+        console.log(`\n ${sym.warning} ${orangeI(` ${printName} `)} ${orange(msg)}\n`);
     }
     if(type === 'info'){
-        console.log(`\n ${sym.info} ${chalk.blue.bold.inverse(msg)}\n`);
+        console.log(`\n ${sym.info} ${blueI(` ${printName} `)} ${blue(msg)}\n`);
     }
 };
    
--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const alert = require('./index.js');
 
 console.clear();
 alert();
-alert({type: 'success', msg: 'All done!'});
+alert({type: 'success', msg: 'All done!', name: 'DONE'});
 alert({type: 'info', msg: 'No info for the moment!'});
 alert({type: 'warning', msg: 'You did add nothing !'});
 alert({type: 'error', msg: 'Something broke'});
